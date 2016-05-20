a9dc609f20be550bcc17c3759c8efe6b5eb5a577
osztrovszkij <v.osztrovszkij@mail.com>
2016-05-20 03:09:14 +0300

    1

--- projectx/src/main/java/dao/mysqlimpl/UserSqlStatement.java
+++
@@ -1,8 +0,0 @@
-package dao.mysqlimpl;
-
-/**
- * Created by roski on 20.5.16.
- */
-public class UserSqlStatement {
-    final static String SELECT_QUERY = "SELECT login, password, roles.role FROM projectx.users JOIN projectx.roles ON roles.id_roles = users.roles_id_roles;";
-}
